645487d78f6c4eac9922220a1770ef36380fe779
Matt Lawson <mattlawson.org@gmail.com>
2021-12-09 14:12:11 +0900

    Update 0-preinstall.sh

--- 0-preinstall.sh
+++ 0-preinstall.sh
@@ -67,15 +67,15 @@ fi
 echo -e "\nCreating Filesystems...\n$HR"
 if [[ ${DISK} =~ "nvme" ]]; then
 mkfs.vfat -F32 -n "EFIBOOT" "${DISK}p2"
-mkfs.btrfs -L "ROOT" "${DISK}p3" -f
-mount -t btrfs "${DISK}p3" /mnt
+mkfs.ext4 -L "ROOT" "${DISK}p3" -f
+mount -t ext4 "${DISK}p3" /mnt
 else
 mkfs.vfat -F32 -n "EFIBOOT" "${DISK}2"
-mkfs.btrfs -L "ROOT" "${DISK}3" -f
-mount -t btrfs "${DISK}3" /mnt
+mkfs.ext4 -L "ROOT" "${DISK}3" -f
+mount -t ext4 "${DISK}3" /mnt
 fi
-ls /mnt | xargs btrfs subvolume delete
-btrfs subvolume create /mnt/@
+ls /mnt | xargs ext4 subvolume delete
+ext4 subvolume create /mnt/@
 umount /mnt
 ;;
 *)
@@ -87,7 +87,7 @@ reboot now
 esac
 
 # mount target
-mount -t btrfs -o subvol=@ -L ROOT /mnt
+mount -t ext4 -o subvol=@ -L ROOT /mnt
 mkdir /mnt/boot
 mkdir /mnt/boot/efi
 mount -t vfat -L EFIBOOT /mnt/boot/
